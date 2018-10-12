003a466b1e715f7ee9336dc13f26faf7e4337255
Brad Westfall <brad@azpixels.com>
2018-10-12 00:05:45 -0700

    docs

--- README.md
+++ README.md
@@ -1,3 +1,43 @@
 # React Flex Columns
 
-Inspired by https://philipwalton.github.io/solved-by-flexbox/demos/grids/ and https://css-tricks.com/dont-overthink-flexbox-grids/
+React Flex Columns is an abstraction on CSS Flexbox-style columns for add-hoc layout that are not complex enough to justify writing custom CSS.
+
+Inspired by https://philipwalton.github.io/solved-by-flexbox/demos/grids and https://css-tricks.com/dont-overthink-flexbox-grids
+
+## Install
+
+```sh
+npm install react-flex-columns
+```
+
+## Basic Usage
+
+```jsx
+import { Columns, Column } from 'react-flex-columns'
+
+const App = () => (
+  <Columns>
+    <Column flex>Left</Column>
+    <Column flex>Right</Column>
+  </Columns>
+)
+```
+
+`<Columns>` creates a `div` with `display: flex`. The children `<Column>`'s must be the first level children of `<Columns>` and create `div`'s that are flex items. Flex items by default will be side-by-side with each other even though the divs are still block-level elements. As flex items, their width will be as wide as their contents unless other properties are provided like `flex: 1`. In the above example, providing a `flex` boolean property will create a `flex: 1` styling which in this case results in two 50% wide columns. [Learning more about Flexbox](https://css-tricks.com/snippets/css/a-guide-to-flexbox/) will help you use this Module.
+
+
+## Examples
+
+See the Codesandbox for an interactive example
+
+![Example Image](docs/example.png)
+
+
+## API
+
+There are two React components exported as named exports:
+
+```sh
+import { Columns, Column } from 'react-flex-columns'
+```
+
--- src/App.js
+++ src/App.js
@@ -7,33 +7,61 @@ const App = () => {
   return (
     <Fragment>
 
-      <Columns gutters middle stackMaxWidth={800}>
-        <Column size={10}>Gutters</Column>
-        <Column flex>
-          <h3>Middle</h3>
-        </Column>
+      <p>
+        Gray and Pink styling added so we can see what's going on.
+        Also note that negative left and right margin on flex container is
+        a <a href="https://csswizardry.com/2011/08/building-better-grid-systems/">technique to help balance gutters</a> and
+        will not inadvertently mess with containers of &lt;Columns>&gt;
+      </p>
+
+      <Columns gutters>
+        <Column flex><h4>First</h4></Column>
+        <Column flex><h4>Second</h4></Column>
+        <Column flex><h4>Third</h4></Column>
+      </Columns>
+      <hr />
+
+      <Columns gutterSize={4}>
+        <Column flex><h4>First</h4></Column>
+        <Column flex><h4>Second</h4></Column>
+        <Column flex><h4>Third</h4></Column>
+      </Columns>
+      <hr />
+
+      <Columns>
+        <Column flex><h4>First</h4></Column>
+        <Column flex><h4>Second</h4></Column>
+        <Column flex><h4>Third</h4></Column>
+      </Columns>
+      <hr />
+
+      <Columns gutters stackMaxWidth={800}>
+        <Column size={10}><h4>First</h4></Column>
+        <Column flex><h4>Second</h4></Column>
       </Columns>
       <hr />
 
       <Columns split>
-        <Column>split</Column>
-        <Column>split</Column>
+        <Column><h4>First</h4></Column>
+        <Column><h4>Second</h4></Column>
       </Columns>
       <hr />
 
-      <Columns gutters middle reverse>
-        <Column size={10}>Gutters</Column>
-        <Column flex>
-          <h3>Middle</h3>
-        </Column>
+      <Columns gutters middle>
+        <Column size={10}><h4>First</h4></Column>
+        <Column flex><span>Second</span></Column>
+      </Columns>
+      <hr />
+
+      <Columns gutters>
+        <Column size={10}><h4>First</h4></Column>
+        <Column flex><span>Second</span></Column>
       </Columns>
       <hr />
 
       <Columns stack>
-        <Column size={10}>Gutters</Column>
-        <Column flex>
-          <h3>Middle</h3>
-        </Column>
+        <Column size={10}><h4>First</h4></Column>
+        <Column flex><h4>Second</h4></Column>
       </Columns>
       <hr />
 
